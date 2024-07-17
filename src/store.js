b0928de15f2983fb73d672eb2cc619f9c4e0be4e
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-07-17 15:42:34 +0200

    installed redux dev tools and solved the currency converter bug

--- src/store.js
+++ src/store.js
@@ -1,4 +1,6 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 import accountReducer from "./features/account/accountSlice.js";
 import customerReducer from "./features/customers/customerSlice.js";
 
@@ -6,6 +8,9 @@ const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
